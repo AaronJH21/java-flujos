1f43471ff73c2f8a9cf223163b0accbc11a32d75
AaronJH21 <93291172+AaronJH21@users.noreply.github.com>
2021-11-24 19:08:53 -0300

    Add files via upload

---
+++ javascrip flujos/script.js
@@ -0,0 +1,12 @@
+console.log("hola, perdon por ser tardar tanto")
+let numero1 = Number(prompt("ingresa un numero menor a 5")) 
+let resultado = (numero1)
+
+console.log("El resultado es :")
+alert("El resultado esta:" )
+if( numero1 ==5 ){
+    alert("mal")
+} else{ 
+    alert("bien")
+
+}
